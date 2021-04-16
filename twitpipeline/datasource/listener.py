eb2e2b7a8c398dc80d4a26392d47a34d65555fda
서현규 <omnipede@coinplug.com>
2021-04-16 15:31:11 +0900

    기능: publisher, data source listener 추가

---
+++ twitpipeline/datasource/listener.py
@@ -0,0 +1,64 @@
+"""
+Foo bar
+"""
+import json
+from abc import ABCMeta, abstractmethod
+
+import tweepy
+from tweepy import StreamListener
+
+from twitpipeline.config import twit_api_key, \
+    twit_api_secret_key, \
+    twit_access_token, \
+    twit_access_token_secret
+from twitpipeline.mq.publisher import Publisher
+
+
+class Listener(metaclass=ABCMeta):
+    """
+    Data source listener interface
+    """
+    @abstractmethod
+    def listen(self) -> None:
+        """
+        Start listening
+        """
+
+
+class TweetListener(Listener):
+    """
+    Listener 구현체
+    """
+    def __init__(self, publisher: Publisher):
+        self._stream_listener: StreamListener = TweetStreamListener(publisher=publisher)
+
+    def listen(self) -> None:
+        """
+        Twitter API 를 통해 데이터를 스트리밍하는 메소드
+        """
+        stream_listener: StreamListener = self._stream_listener
+        tweet_auth = tweepy.OAuthHandler(twit_api_key, twit_api_secret_key)
+        tweet_auth.set_access_token(twit_access_token, twit_access_token_secret)
+        twitter_stream = tweepy.Stream(tweet_auth, stream_listener)
+        twitter_stream.filter(track=['game'])
+
+
+class TweetStreamListener(StreamListener):
+    """
+    tweepy StreamListener 구현체
+    """
+    def __init__(self, publisher: Publisher):
+        super().__init__()
+        self._publisher = publisher
+
+    def on_status(self, status) -> None:
+        tweet = json.dumps({
+            'id': status.id, 'created_at': status.created_at, 'text': status.text
+        }, default=str)
+        self._publisher.publish(data=tweet.encode('utf-8'))
+
+    def on_error(self, status_code):
+        print('ERROR', status_code)
+        if status_code == 420:
+            return False
+        return True
---
+++ twitpipeline/main.py
@@ -0,0 +1,25 @@
+"""
+Main script
+"""
+
+from twitpipeline.datasource.listener import Listener, TweetListener
+from twitpipeline.mq.publisher import Publisher, GooglePubsub
+
+__version__ = "1.0.0"
+
+
+def main():
+    """
+    Main script
+    """
+
+    # Google PUBSUB 으로 데이터를 전송하는 객체
+    publisher: Publisher = GooglePubsub()
+
+    # Twit 데이터를 불러오는 객체. 데이터를 불러온 뒤 바로 publiser 를 이용해 PUBSUB 으로 데이터를 전송함.
+    listener: Listener = TweetListener(publisher)
+    listener.listen()
+
+
+if __name__ == "__main__":
+    main()
